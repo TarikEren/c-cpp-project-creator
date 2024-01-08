185a5c3be4d8095ef8884ba2f33530833b76718c
TarikEren <1210606015@nku.edu.tr>
2024-01-09 00:20:37 +0300

    Minor fix

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, const char** argv) { 
     if (argc == 1) {
         printf("ERROR: Provide a valid argument. Type project-creator -h for help\n");
+        return 1;
     }
     for (int i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "-h")) {
