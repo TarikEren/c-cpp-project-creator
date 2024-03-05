68827e47cabc4ba4eee7ac87d3ebd9976a7b8e84
TarikEren <1210606015@nku.edu.tr>
2024-03-05 16:20:21 +0300

    Added file creation checks

--- src/prompts.cpp
+++ src/prompts.cpp
@@ -15,6 +15,9 @@ void help_prompt() {
 std::filesystem::file_status status = {};
 
 int project_prompt(const char* project_name) {
+    int git_ignore;
+    int file;
+    int makefile;
     if (dir_exists(project_name, status)) {
         printf("ERROR: Path already exists\n");
         return PATH_EXISTS;
@@ -25,19 +28,57 @@ int project_prompt(const char* project_name) {
         int opt;
         std::cout << "1- C Project\n2- C++ Project\n> ";
         std::cin >> opt; 
+
+        //May God forgive me for this abomination I've spawned.
         if (opt == 1) {
-            create_c_project();
-            create_makefile();
-            create_gitignore();
-            std::cout << "\nINFO: Project created successfully." << std::endl;
-            return SUCCESS;
+            file = create_c_project();
+            if (file) {
+                makefile = create_makefile();
+                if (makefile) {
+                    git_ignore = create_gitignore();
+                    if (git_ignore){
+                        std::cout << "\nINFO: Project created successfully." << std::endl;
+                        return SUCCESS;
+                    }
+                    else {
+                        std::cerr << "ERROR: Failed to create .gitignore. Aborting." << std::endl;
+                        return FAILURE;
+                    }
+                }
+                else {
+                    std::cerr << "ERROR: Failed to create makefile. Aborting." << std::endl;
+                    return FAILURE;
+                }
+            }
+            else {
+                std::cerr << "ERROR: Failed to create main.c. Aborting." << std::endl;
+                return FAILURE;
+            }
         }
         else if (opt == 2) {
-            create_cpp_project();
-            create_cmakelists();
-            create_gitignore();
-            std::cout << "\nINFO: Project created successfully." << std::endl;
-            return SUCCESS;
+            file = create_cpp_project();
+            if (file) {
+                makefile = create_cmakelists();
+                if (makefile) {
+                    git_ignore = create_gitignore();
+                    if (git_ignore){
+                        std::cout << "\nINFO: Project created successfully." << std::endl;
+                        return SUCCESS;
+                    }
+                    else {
+                        std::cerr << "ERROR: Failed to create .gitignore. Aborting." << std::endl;
+                        return FAILURE;
+                    }
+                }
+                else {
+                    std::cerr << "ERROR: Failed to create CMakeLists. Aborting." << std::endl;
+                    return FAILURE;
+                }
+            }
+            else {
+                std::cerr << "ERROR: Failed to create main.cpp. Aborting." << std::endl;
+                return FAILURE;
+            }
         }
         else {
             std::cout << "ERROR: Wrong input" << std::endl;
